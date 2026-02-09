44bb1f4c565049f669afb16a19fbb00fb2887958
Thomas McKee <caffeine.storm@gmail.com>
2026-02-09 14:47:25 -0500

    add some makefile helpers for finding a self-update version target

--- dist/self-update.mk
+++ dist/self-update.mk
@@ -1,6 +1,7 @@
 SHELL:=/bin/bash
 
 base-rev-version:=${file <VERSION}
+base-rev-major-version:=$(firstword $(subst ., ,${base-rev-version}))
 base-rev-hash:=${file <VERSION.hash}
 
 ifneq (,$(findstring s,${MAKEFLAGS}))
@@ -16,10 +17,21 @@ ifneq "${live-dir}" "build"
 $(error "self-update is disabled unless the containing directory is called 'build'")
 endif
 
+# For tests, we can use '.' for fetching tag info just from the local git repo.
+# repo-url?=https://github.com/caffeine-storm/buildlib
+repo-url?=.
+
+list-major-version-tags:=git ls-remote --tags "${repo-url}" 'v${base-rev-major-version}\.*'
+git-tag-to-version:=sed -e 's,.*/\(v[0-9.]*\)^{}$$,\1,'
+order-by-version-descending:=sort -Vr
+take-first:=head -1
+repo-latest-version=$(shell ${list-major-version-tags} | ${git-tag-to-version} | ${order-by-version-descending} | ${take-first})
+
 # TODO(tmckee): stubbing upstream tarball fetch until we figure out release
 # strategy. After that, we can add CI to buildlib. The tests can also specify a
 # specific upstream_url to use thereby avoiding a network fetch during test.
 upstream_url?=file://$(abspath ./../../buildlib-v0.0.2.tar.gz)
+
 self-update: no-local-changes
 	curl ${silence} -L ${upstream_url} -o buildlib-upstream.tar.gz
 	tar -xf buildlib-upstream.tar.gz --wildcards '*/VERSION' -O > VERSION.upstream
