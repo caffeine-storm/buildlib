e8dc506c5e49112b29547b0b8869869268e3eb22
Thomas McKee <caffeine.storm@gmail.com>
2026-02-10 13:40:02 -0500

    mark directories under 'dist/' as pre-reqs for deciding which files need to be tarball'd
    That way, adding or removing .mk files will trigger re-builds appropriately

--- dist/hash.mk
+++ dist/hash.mk
@@ -1,8 +1,14 @@
 SHELL:=/bin/bash
 
 distdir?=.
+distdirs:=${shell find ${distdir} -type d | sort}
+
 dotmakes:=${shell find ${distdir} -name '*.mk' -type f | sort}
 dotmakenames:=${dotmakes:${distdir}/%=%}
+
+# If a file or subdirectory gets created or removed, we can notice it by
+# checking directory modification times.
+${distdir}/local.hash: ${distdirs}
 
 ${distdir}/local.hash: ${dotmakes}
 	# Hash each of the files so that we can see what changed file-by-file. This
