d11975389b9b13593ae4660b372f13a1a7079bf0
Thomas McKee <caffeine.storm@gmail.com>
2026-02-13 22:17:49 -0500

    add an example Go project for testing buildlib

---
+++ test/example/foo.go
@@ -0,0 +1,6 @@
+// an example package for use during tests of build scripts
+package foo
+
+func Foo() {
+
+}
---
+++ test/example/foo_test.go
@@ -0,0 +1,11 @@
+package foo_test
+
+import (
+	"testing"
+
+	"example.com/example"
+)
+
+func TestFoo(t *testing.T) {
+	foo.Foo()
+}
